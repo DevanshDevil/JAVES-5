34a0e5fd7c8d129e0cbeba82e9230e78be60174e
SHIVANSH <75574324+Hellboy-Shivansh@users.noreply.github.com>
2021-06-16 19:52:28 +0530

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -4,7 +4,7 @@ from telethon import version
 from userbot import (HEROKU_APP_NAME, HEROKU_API_KEY, CMD_HELP, BOTLOG, BOTLOG_CHATID, JAVES_NAME, ALIVE_NAME, YOUR_SHORT_NAME)
 from userbot.events import javes05, rekcah05 
 import os
-shivam = os.environ.get("ALIVE_PIC","https://telegra.ph/file/8a3ab7aa9d71c29aae187.mp4")
+shivam = os.environ.get("ALIVE_PIC","https://telegra.ph/file/169b98cc836367751dcb1.jpg")
 
 import asyncio
 from telethon import events
@@ -32,7 +32,7 @@ S3_NAME = os.environ.get("S3_NAME", JAVES_NNAME)
 S2_USER = os.environ.get("S2_USER", DEFAULTUSER)
 S3_USER = os.environ.get("S3_USER", DEFAULTUSER)
 
-v = "2.0.0"
+v = "5.0.0"
 
 if rksu:
  if FULL_SUDO:
